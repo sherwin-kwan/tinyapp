da031bf31ad93466549ed4c0b7d0c0ec447ded18
Sherwin <sherwin.kwan92@gmail.com>
2020-09-28 16:07:58 -0700

    started doing some interpolations with variables in the ejs files

--- index.js
+++ index.js
@@ -24,12 +24,29 @@ app.get('/hello.html', (req, res) => {
   res.send('<html><head><title>Hello world</title></head><body>Hello world</body></html>');
 })
 
+app.get('/about', (req, res) => {
+  res.render('/about.ejs');
+})
+
+app.get('/testing.html', (req, res) => {
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
+    { name: 'Tux', organization: "Linux", birth_year: 1996 },
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot.";
+  res.render('index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+  res.render('index.ejs');
+})
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
- 
+});
